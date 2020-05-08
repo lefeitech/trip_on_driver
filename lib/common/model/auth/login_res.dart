aef75f65b0850705d21366e70bbd5ae3f548da15
mac <mac@macdeiMac.lan>
2020-05-09 00:14:01 +0800

    feat:add login res model

---
+++ lib/common/model/auth/login_res.dart
@@ -0,0 +1,69 @@
+import 'dart:convert' show json;
+
+import 'package:driver/common/model/user/user_info.dart';
+
+T asT<T>(dynamic value) {
+  if (value is T) {
+    return value;
+  }
+
+  return null;
+}
+
+class LoginRes {
+  LoginRes({
+    this.code,
+    this.msg,
+    this.data,
+  });
+
+  factory LoginRes.fromJson(Map<String, dynamic> jsonRes) => jsonRes == null
+      ? null
+      : LoginRes(
+          code: asT<int>(jsonRes['code']),
+          msg: asT<String>(jsonRes['msg']),
+          data: LoginData.fromJson(asT<Map<String, dynamic>>(jsonRes['data'])),
+        );
+
+  int code;
+  String msg;
+  LoginData data;
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'code': code,
+        'msg': msg,
+        'data': data,
+      };
+
+  @override
+  String toString() {
+    return json.encode(this);
+  }
+}
+
+class LoginData {
+  LoginData({
+    this.token,
+    this.info,
+  });
+
+  factory LoginData.fromJson(Map<String, dynamic> jsonRes) => jsonRes == null
+      ? null
+      : LoginData(
+          token: asT<String>(jsonRes['token']),
+          info: UserInfoModel.fromJson(asT<Map<String, dynamic>>(jsonRes['info'])),
+        );
+
+  String token;
+  UserInfoModel info;
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+        'token': token,
+        'info': info,
+      };
+
+  @override
+  String toString() {
+    return json.encode(this);
+  }
+}
